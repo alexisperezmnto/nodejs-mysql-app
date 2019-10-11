efea477d567d665c15dd23abde5f006ad11b595b
alexis <alexisperezmnto@gmail.com>
2019-10-11 14:46:09 -0500

    Se puede guardar en la base de datos y mostrar los registros

--- src/routes/links.js
+++ src/routes/links.js
@@ -7,7 +7,25 @@ router.get('/add', (req, res) => {
 	res.render('links/add');
 });
 
-router.post('/add', (req, res) => {
-	res.send('Recibido');
-})
+router.post('/add', async (req, res) => {
+	const { title, url, description } = req.body;
+	const newLink = {
+		title,
+		url,
+		description
+	};
+
+	await pool.query('INSERT INTO links set ?', [newLink]);
+	// Escribir en la consola de MySQL:
+	// ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'root';
+	// Para evitar el error que aparece "UnhandledPromiseRejectionWarning"
+
+	res.redirect('/links');
+});
+
+router.get('/', async (req, res) => {
+	const links = await pool.query('SELECT * FROM links');
+	res.render('links/list', {links});
+});
+
 module.exports = router;
